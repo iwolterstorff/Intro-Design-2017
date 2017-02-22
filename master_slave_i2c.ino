3386f443cb1385ead9024fc1de35b33c71548da1
Ian Wolterstorff <ianwolt@yahoo.com>
2017-02-22 13:27:24 -0500

    Initial commit of I2C code

---
+++ master_slave_i2c.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+
+#define NUM_CHARS 4
+
+//I am sending/slave
+
+//char turn[NUM_CHARS];
+int turn = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(12);
+  Wire.onRequest(requestEvent);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(100);
+//  Serial.readBytes(turn, NUM_CHARS);
+  if (Serial.available() > 0) {
+      turn = Serial.parseInt();
+    }
+  Serial.println(turn);
+}
+
+void requestEvent() {
+//  memset(chars, 0, NUM_CHARS);
+  Wire.write(turn);
+}
+
